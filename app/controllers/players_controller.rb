c0204646021d2fcc87bf75ce02f7122a5f830728
M Deri Taufan <deritaufan@gmail.com>
2016-10-23 11:27:54 +0200

    Add editing player information

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -11,9 +11,24 @@ class PlayersController < ApplicationController
   def create
   	@player = Player.new(player_params)
   	if @player.save
+  		flash[:success] = "The Player successfully created!"
   		redirect_to players_url
   	else
   		render 'new'
+  	end
+  end
+
+  def edit
+  	@player = Player.find(params[:id])
+  end
+
+  def update
+  	@player = Player.find(params[:id])
+  	if @player.update_attributes(player_params)
+  		flash[:success] = "The Player information successfully updated."
+  		redirect_to players_url
+  	else
+  		render 'edit'
   	end
   end
 
